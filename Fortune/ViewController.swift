fd5d7e7588ff04a14e64a478a3d06a23fc2b3b3e
essochi <rrr.www.ppp@gmail.com>
2018-07-12 01:12:39 +0900

    Initial Commit

---
+++ Fortune/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fortune
+//
+//  Created by koya s on 2018/07/12.
+//  Copyright © 2018 essochi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
